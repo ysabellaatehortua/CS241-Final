85dead3bfaa9a20e5a435323b65b39181edf069b
Greg Gillen <ggillen@oberlin.edu>
2021-04-09 15:09:27 -0400

    Testing that c++ compiles via Clang++. Function prototypes started.

---
+++ cPlus.cc
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(){
+
+printf("hello, c plus plus. Compiled via Clang++\n");
+
+}
---
+++ dict.cc
@@ -0,0 +1,6 @@
+// Dictionary class
+// Functions: 
+// MakeDict (void) instantiates the class with counts of zero for function words
+// AddWord (void) takes a string increments the count by one for that word
+//
+
