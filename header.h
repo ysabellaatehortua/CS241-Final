f8b5fb9c435d7de997940934fa0bf1b54edb312f
Nadine Connamacher <nconnama@oberlin.edu>
2021-05-08 00:45:48 -0400

    better header

--- header.h
+++ header.h
@@ -1,11 +1,15 @@
+#include <iostream>
+#include <map>
+#include <string>
+#include <array>
+
 #ifndef HEADER_H_
 #define HEADER_H_
 
-//need to change main functions in dict and readin to "dict()" and "readin()"
+using namespace std;
+map<const string, int> createDict (std::string text);
 
-int dict();
-
-int readin();
+void addWord(map<const string, int>& mapInput, string word);
 
 
 #endif
